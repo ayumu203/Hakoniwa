2d3bc41dc4119dccc71b880d14778517ba34ef7f
ayumu203 <ochiohita@gmail.com>
2024-11-05 12:49:15 +0900

    ChakraUI導入のために種々の設定

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
+import { ChakraProvider,createSystem, defineConfig  } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+})
+
+const system = createSystem(config);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
